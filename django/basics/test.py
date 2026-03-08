a2d5ae3846644d1746ef66a5982a7e6a4c229076
Eric <ericjaucian@gmail.com>
2026-03-08 08:11:57 +0800

    first commit

---
+++ django/basics/test.py
@@ -0,0 +1,9 @@
+data = {"hey": "hi",
+        "package":{"version": "1.0.0",
+                   "dependencies": {"numpy": "1.21.0"}   
+                   }}
+
+file = {"data": data,
+        "metadata": {"author": "John Doe", 
+                     "version": "1.0.0"}                
+        }
